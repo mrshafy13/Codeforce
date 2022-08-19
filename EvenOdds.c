fcdfebfa867fd5a91d4b67cffac95c2458052f4d
Muhibur Shafy <shafy1559@gmail.com>
2022-08-19 20:52:39 +0600

    solved on 19/08/2022

---
+++ EvenOdds.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main () 
+{
+    long long int n, k, x, i, j;
+    scanf("%lld", &n);
+    scanf("%lld", &k);
+
+    //for even numbers;
+    if(n%2 == 0)
+    {
+        x = n/2;
+        if(k<=x)
+        {
+            i = 2*k - 1;
+        }
+        else if(k>x)
+        {
+            k = k - x;
+            i = 2*k;
+        }
+    }
+    else if(n%2!=0)
+    {
+        x = n/2 + 1;
+        if(k<=x)
+        {
+            i = 2*k - 1;
+        }
+        else if(k>x)
+        {
+            k = k - x;
+            i = 2*k;
+        }
+        
+    }
+    printf("%lld\n", i);
+    return 0;
+}
